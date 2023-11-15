f8f58413397b182c8b29504aca7a1bd52b0f34c0
Emeka Umejiofor <pumej1985@gmail.com>
2023-11-15 00:48:22 -0500

    added files for jenkins project using terraform

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Data source for our SSH key
+
+data "digitalocean_ssh_key" "pumej-dokey" {
+  name = "pumej-dokey"
+}
+
+# Creating a Digital Ocean Droplet
+resource "digitalocean_droplet" "pumej-droplet" {
+  image    = var.image
+  name     = var.droplet_name
+  region   = var.droplet_region
+  size     = var.image_size
+  ssh_keys = [data.digitalocean_ssh_key.pumej-dokey.id]
+}
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+droplet_name = "jenkins-server"
+droplet_region = "nyc1"
+droplet_size = "s-2vcpu-4gb"
+droplet_image = "ubuntu-22-04-x64"
+do_token = "dop_v1_3ca9a147e4409acd29c5f7fad1db18eac15a65ba6c1de4183ed60ce782cdc985"
